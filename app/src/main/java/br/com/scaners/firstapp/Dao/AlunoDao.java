c8ee82a1e6be9377f2501aa20ed6980a5a9a8842
Rafael Scane <rafaelscane@gmail.com>
2016-07-11 14:04:54 -0300

    commit inicial do projeto

---
+++ app/src/main/java/br/com/scaners/firstapp/Dao/AlunoDao.java
@@ -0,0 +1,85 @@
+package br.com.scaners.firstapp.Dao;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.NonNull;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.scaners.firstapp.modelo.Aluno;
+
+/**
+ * Created by Scaners on 09/07/2016.
+ */
+public class AlunoDao extends SQLiteOpenHelper{
+
+    public AlunoDao(Context context) {
+        super(context, "Agenda" , null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String sql = "CREATE TABLE ALUNOS (id INTEGER PRIMARY KEY AUTO INCREMENT, nome TEXT NOT NULL, endereco TEXT, telefone TEXT, site TEXT, nota REAL);";
+        sqLiteDatabase.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        String sql = "DROP TABLE IF EXISTS ALUNOS";
+        sqLiteDatabase.execSQL(sql);
+        onCreate(sqLiteDatabase);
+    }
+
+    public void insert(Aluno aluno) {
+        SQLiteDatabase writableDatabase = getWritableDatabase();
+        ContentValues contentValues = getContentValues(aluno);
+        writableDatabase.insert("Alunos", null,contentValues);
+    }
+
+    public List<Aluno> buscaAlunos() {
+        SQLiteDatabase conn =  getReadableDatabase();
+        Cursor cursor = conn.rawQuery("SELECT * FROM ALUNOS", null);
+        List<Aluno> alunos = new ArrayList<Aluno>();
+        while (cursor.moveToNext()){
+            Aluno aluno = new Aluno();
+            aluno.setId(cursor.getLong(cursor.getColumnIndex("id")));
+            aluno.setNome(cursor.getString(cursor.getColumnIndex("nome")));
+            aluno.setEndereco(cursor.getString(cursor.getColumnIndex("endereco")));
+            aluno.setTelefone(cursor.getString(cursor.getColumnIndex("telefone")));
+            aluno.setSite(cursor.getString(cursor.getColumnIndex("site")));
+            aluno.setNota(cursor.getDouble(cursor.getColumnIndex("nota")));
+            alunos.add(aluno);
+        }
+        cursor.close();
+        return alunos;
+    }
+
+    public void delete(Aluno aluno) {
+        SQLiteDatabase writableDatabase = getWritableDatabase();
+        String[] parameters = {aluno.getId().toString()};
+        writableDatabase.delete("ALUNOS", "id = ?", parameters);
+    }
+
+    public void update(Aluno aluno) {
+        SQLiteDatabase writableDatabase = getWritableDatabase();
+        ContentValues contentValues = getContentValues(aluno);
+        String[] parameters = {aluno.getId().toString()};
+        writableDatabase.update("ALUNOS", contentValues, "id = ?", parameters);
+    }
+
+    @NonNull
+    private ContentValues getContentValues(Aluno aluno) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("nome", aluno.getNome());
+        contentValues.put("endereco", aluno.getEndereco());
+        contentValues.put("telefone",  aluno.getTelefone());
+        contentValues.put("site", aluno.getSite());
+        contentValues.put("nota", aluno.getNota());
+        return contentValues;
+    }
+}
---
+++ app/src/main/java/br/com/scaners/firstapp/FormularioActivity.java
@@ -0,0 +1,94 @@
+package br.com.scaners.firstapp;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.SystemClock;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.Serializable;
+
+import br.com.scaners.firstapp.Dao.AlunoDao;
+import br.com.scaners.firstapp.modelo.Aluno;
+
+public class FormularioActivity extends AppCompatActivity {
+    public static final int INTENT_CAMERA = 1;
+    private FormularioHelper formularioHelper;
+    private String caminhoFoto;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+        formularioHelper = new FormularioHelper(this);
+        Intent intent = getIntent();
+        Aluno aluno = (Aluno) intent.getSerializableExtra("aluno");
+        if (aluno != null){
+            formularioHelper.carregaFormulario(aluno);
+        }
+
+        Button botaoFoto = (Button) findViewById(R.id.formulario_btn_foto);
+        botaoFoto.setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View view) {
+                Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                caminhoFoto = getExternalFilesDir(null)+"/"+ System.currentTimeMillis()+".jpg";
+                File arquivoFoto = new File(caminhoFoto);
+                intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(arquivoFoto));
+                startActivityForResult(intentCamera, INTENT_CAMERA);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if ( requestCode == INTENT_CAMERA && (resultCode == Activity.RESULT_OK)){
+            ImageView viewFoto = (ImageView) findViewById(R.id.formulario_foto);
+            Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+            Bitmap bitmapFormatado = Bitmap.createScaledBitmap(bitmap, 300, 300, true);
+            viewFoto.setImageBitmap(bitmapFormatado);
+            viewFoto.setScaleType(ImageView.ScaleType.FIT_XY);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_formulario, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menu_formulario_ok :
+                Aluno aluno = formularioHelper.getAluno();
+                AlunoDao alunoDao = new AlunoDao(this);
+                if (aluno.getId() != null) {
+                    alunoDao.update(aluno);
+                }else {
+                    alunoDao.insert(aluno);
+                }
+                alunoDao.close();
+                Toast.makeText(FormularioActivity.this, "Aluno " +aluno.getNome()+ " cadastrado!", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
